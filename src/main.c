b235256299164d39a72fa3ad38b65451d14865cd
cuki2 <mauricio@krebs.com.br>
2015-11-27 16:54:31 -0200

    block write bug

--- src/main.c
+++ src/main.c
@@ -20,10 +20,12 @@
 #define READ_OCR 58
 #define CRC_ON_OFF 59
 
-#define IDLE_TOKEN 0x01
-#define DATA_START_TOKEN 0xFE
+#define IDLE_TOKEN			0x01
+#define DATA_START_TOKEN	0xFE
+#define DUMMY_BYTE			0xFF
 
-#define MMCSD_MAX_BLOCK_SIZE 512
+#define MMCSD_MAX_BLOCK_SIZE	512
+#define BUFFER_SIZE				MMCSD_MAX_BLOCK_SIZE
 
 #define _SS PIN_A5
 
@@ -40,7 +42,7 @@ enum MMCSD_err {
 };
 
 void mmcsd_deselect(void) {
-	spi_write(0xFF);
+	spi_write(DUMMY_BYTE);
 	output_high(_SS);
 }
 
@@ -84,7 +86,7 @@ int mmcsd_send_cmd(int cmd, long long arg, short g_CRC_enabled) {
 	if (g_CRC_enabled)
 		packet[5] = mmcsd_crc7(packet, 5);
 	else
-		packet[5] = 0xFF;
+		packet[5] = DUMMY_BYTE;
 
 	spi_write(packet[0]);
 	spi_write(packet[1]);
@@ -100,8 +102,8 @@ int mmcsd_get_r1(void) {
 	int response = 0, timeout = 0xFF;
 
 	while (timeout) {
-		response = spi_read(0xFF);
-		if (response != 0xFF)
+		response = spi_read(DUMMY_BYTE);
+		if (response != DUMMY_BYTE)
 			return response;
 		timeout--;
 	}
@@ -112,7 +114,7 @@ int mmcsd_get_r1(void) {
 int mmcsd_get_r2(int *r2) {
 
 	r2[1] = mmcsd_get_r1();
-	r2[0] = spi_read(0xFF);
+	r2[0] = spi_read(DUMMY_BYTE);
 	return 0;
 }
 
@@ -122,7 +124,7 @@ int mmcsd_get_r7(int *r7) {
 	r7[4] = mmcsd_get_r1();
 
 	for (i = 0; i < 4; i++)
-		r7[3 - i] = spi_read(0xFF);
+		r7[3 - i] = spi_read(DUMMY_BYTE);
 
 	return r7[4];
 }
@@ -185,7 +187,7 @@ int mmcsd_init(int *r7) {
 
 	mmcsd_deselect();
 	for (i = 0; i < 10; ++i)
-		spi_read(0xFF);
+		spi_read(DUMMY_BYTE);
 
 	delay_ms(10);
 	mmcsd_select();
@@ -237,25 +239,90 @@ int mmcsd_read_single_block(long long address) {
 	return mmcsd_get_r1();
 }
 
+int mmcsd_set_blocken(long long address, long size) {
+
+	mmcsd_send_cmd(SET_BLOCKLEN, size, FALSE);
+	return mmcsd_get_r1();
+}
+
 int mmcsd_read_block(long long address, int *data, long size) {
 	int r1, data_token;
 	long i;
 
-	r1 = mmcsd_read_single_block(address);
+//	r1 = mmcsd_set_blocken(address, size);
+//	if (r1 != 0)
+//		return r1;
 
+	r1 = mmcsd_read_single_block(address);
 	if (r1 != 0)
 		return r1;
 
 	do {
-		data_token = spi_read(0xFF);
-	} while (data_token != 0xFE);
+		data_token = spi_read(DUMMY_BYTE);
+	} while (data_token != DATA_START_TOKEN);
 
 	for (i = 0; i < size; ++i) {
-		data[i] = spi_read(0xFF);
+		data[i] = spi_read(DUMMY_BYTE);
 	}
 
-	spi_write(0xFF);
-	spi_write(0xFF); //CRC
+	spi_write(DUMMY_BYTE);
+	spi_write(DUMMY_BYTE); //CRC
+
+	return r1;
+}
+
+int mmcsd_write_single_block(long long address) {
+
+	mmcsd_send_cmd(READ_SINGLE_BLOCK, address, FALSE);
+	return mmcsd_get_r1();
+}
+
+int mmcsd_write_block(long long address, long size, int *data, int *err) {
+	int r1;
+	long i;
+
+//	delay_ms(100);
+//	mmcsd_select();
+//	r1 = mmcsd_set_blocken(address, MMCSD_MAX_BLOCK_SIZE);
+//	mmcsd_deselect();
+
+//	if (r1)
+//		return 1;
+
+	delay_ms(100);
+	mmcsd_select();
+	r1 = mmcsd_write_single_block(address);
+
+	if (r1) {
+		mmcsd_deselect();
+		*err = 1;
+		return r1;
+	}
+
+	spi_write(DUMMY_BYTE);
+	spi_write(DATA_START_TOKEN);
+
+	for (i = 0; i < MMCSD_MAX_BLOCK_SIZE; ++i) {
+		if (size) {
+			spi_write(data[i]);
+			size--;
+		} else
+			spi_write(0);
+	}
+
+	spi_write(DUMMY_BYTE);
+	spi_write(DUMMY_BYTE);
+	r1 = mmcsd_get_r1();
+
+	if (r1) {
+		*err = 2;
+		return r1;
+	}
+
+	while (spi_read(DUMMY_BYTE) == 0)
+		;
+
+	mmcsd_deselect();
 
 	return r1;
 }
@@ -263,9 +330,10 @@ int mmcsd_read_block(long long address, int *data, long size) {
 int main(void) {
 
 	long cont;
-	int r7[5] = { 0, 0, 0, 0, 0 }, data[MMCSD_MAX_BLOCK_SIZE];
-	int r;
-	short shoot;
+	int r7[5] = { 0, 0, 0, 0, 0 }, data[BUFFER_SIZE];
+	int teste[8] = { 'm', 'a', 'u', 'r', 'i', 'c', 'i', 'o' };
+	int r, err;
+	short shoot, shoot1;
 
 	spi_init(SPI, TRUE);
 	printf("\n\rSDCard");
@@ -288,21 +356,38 @@ int main(void) {
 					shoot = FALSE;
 					delay_ms(100);
 					mmcsd_select();
-					r = mmcsd_read_block(0, &data, MMCSD_MAX_BLOCK_SIZE);
+					r = mmcsd_read_block(0, &data, BUFFER_SIZE);
 					mmcsd_deselect();
-					printf("\n\rTeste Leitura 0x%X %s\n\r", r,
-							(int *) r ? "Error" : "Sucess");
+					printf("\n\rTeste Leitura 0x%X", r);
 					if (!r) {
-						for (cont = 0; cont < MMCSD_MAX_BLOCK_SIZE; ++cont) {
-							if (!(cont % 16) && cont)
+						printf("...ok. Lendo %lu posicoes...", BUFFER_SIZE);
+						for (cont = 0; cont < BUFFER_SIZE; ++cont) {
+							if (!(cont % 8))
 								printf("\n\r");
-							printf(" 0x%X", data[cont]);
+							printf(" %03lu:0x%X", cont, data[cont]);
 						}
-					}
+					} else
+						printf("...falha!!!!");
 				}
 			}
 		} else if (!shoot)
 			shoot = TRUE;
+
+		if (!input(PIN_B1)) {
+			delay_ms(100);
+			if (!input(PIN_B1)) {
+				if (shoot1) {
+					shoot1 = FALSE;
+					r = mmcsd_write_block(0, 8, teste, &err);
+					printf("\n\rTeste Escrita 0x%X %u", r, err);
+//					if (!r)
+//						printf("...ok.");
+//					else
+//						printf("...falha!!!!");
+				}
+			}
+		} else if (!shoot1)
+			shoot1 = TRUE;
 	}
 
 	return 0;
